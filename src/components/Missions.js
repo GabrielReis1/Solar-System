3f5b291cc9b4d94e1c45532603c9ea8b23667218
GabrielReis1 <gabrielreis.santos@outlook.com>
2023-01-24 16:15:22 -0300

    requisito 7

---
+++ src/components/Missions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Missions() {
+  return (
+    <div data-testid="missions">
+      {/* conteudo adicional aqui */}
+    </div>
+  );
+}
+
+export default Missions;
