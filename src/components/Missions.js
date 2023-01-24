b6121c7045db18219f27d9157e424d988d7b187b
GabrielReis1 <gabrielreis.santos@outlook.com>
2023-01-24 16:43:55 -0300

    requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
+import missions from '../data/missions';
 
-function Missions() {
-  return (
-    <div data-testid="missions">
-      <Title headline="Missões" />
-    </div>
-  );
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {missions.map((mission) => (
+          <MissionCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Missions;
